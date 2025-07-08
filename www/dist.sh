2db5143d12f93293e3c438d96d7440f97628d414
Kindy Lin <kindy.lin@gmail.com>
2025-07-08 11:32:31 +0800

    dist.sh: cp deps instead of .tar, support multiple pkgs

--- www/dist.sh
+++ www/dist.sh
@@ -1,6 +1,11 @@
 set -e # fail on error
 set -x # echo commands
 
-rm -rf dist && bun build --target=bun --production --outdir=dist index.ts && cp sources.tar dist/
+rm -rf dist
+bun build --target=bun --production --outdir=dist index.ts
+cp -r pkgs dist/
 
-docker build -t zdocs:0.0.1 .
+docker build -t zdocs:0.0.2-pre .
+
+# manual tag & push
+# docker tag zdocs:0.0.1 ghcr.io/hon-sh/zdocs:0.0.1
